cdb3b4aeab4a15083618df8cd6ce70ed5d0ae456
bujidefeng <1587770721@qq.com>
2021-01-13 10:42:46 +0800

    创建一个类

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,2 +1,11 @@
+/**
+ * @author chenyuzhu
+ * @ClassNmae Cat
+ * @Description
+ * @data 20200113
+ */
 public class Cat {
+    public static void main(String[] args) {
+        System.out.println("I am a cat");
+    }
 }
